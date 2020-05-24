e5b4a62236ec267892535c1954178e76656c182f
deng <6882855@qq.com>
2020-05-24 19:44:55 +0800

    test

--- src/test/alchemy_test.py
+++ src/test/alchemy_test.py
@@ -18,24 +18,34 @@ class Test(Base):
     __tablename__ = 'test'
     test_id = Column(Integer, primary_key=True)
     test_name = Column(String)
-    test_value = Column(String, nullable=True)
-
+    test_value = Column(String)
+    test_value2 = Column(String)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-r = session.query(Test).from_statement(
-    text("SELECT test.*, 'abcefg' test_value FROM test where test_id=:id")).\
+
+r = session.query(Test, Test.test_value.label("test_value"),
+        Test.test_value2.label("test_value2")) .from_statement(
+    text("SELECT test.*, 'abcefg' test_value, 'abcef2g' test_value2 FROM test where test_id=:id")).\
     params(id=1).all()
 
-t = Test(test_id=None, test_name="abc")
 
-print("------------")
-print(t.__dict__)
-for i in t.__dict__:
-    print(type(getattr(t, i)) is not state.InstanceState)
-print("============")
-# session.commit()
+session.execute("insert into test(test_name) values(:test_name)", {"test_name":"sxs's"})
+
+session.commit()
+
+
+#
+# t = Test(test_id=None, test_name="abc")
+# print("------------")
+# print(t.__dict__)
+# for i in t.__dict__:
+#     if type(getattr(t, i)) is state.InstanceState:
+#         continue
+#     print(getattr(Test, i).primary_key)
+# print("============")
+
 
 
 # t = Test(test_id=None, test_name="abc")
