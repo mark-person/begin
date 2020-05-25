52dda3f37f8548295ffbdaa0f1d063f705ea6b1e
deng <6882855@qq.com>
2020-05-25 23:18:23 +0800

    test

--- src/test/alchemy_test.py
+++ src/test/alchemy_test.py
@@ -1,11 +1,15 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, text
+from sqlalchemy import Column, Integer, String, text, Date, DateTime, DECIMAL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, state
 
+from decimal import *
+from datetime import *
+
 from src.test.test import Test
+
 import json
 
 from datetime import datetime
@@ -15,17 +19,22 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 
 class AlchemyEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    if isinstance(data, datetime):
-                        data=data.strftime('%Y-%m-%d %H:%M:%S')
-                    json.dumps(data)
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
+        if not isinstance(obj.__class__, DeclarativeMeta):
+            return None
+        fields = {}
+        for field in obj.__dict__:
+            field_val = getattr(obj, field)
+            if field_val is None or type(field_val) is state.InstanceState:
+                continue
+            if type(field_val) is datetime:
+                field_val = field_val.strftime('%Y-%m-%d %H:%M:%S')
+            elif type(field_val) is date:
+                field_val = field_val.strftime('%Y-%m-%d')
+            elif type(field_val) is Decimal:
+                print(field_val)
+                field_val = '%.2f' % field_val
+            fields[field] = field_val
+        return fields
 
 
 engine = create_engine(
@@ -45,10 +54,9 @@ r = session.query(Test).from_statement(
     params(id=29).all()
 # t = Test(test_id=None, test_name="abc")
 # print(t)
-print(len(r))
-print(type(r))
+# print(len(r))
+print("---------begin")
+print(r)
+print("---------end")
 
-Hosts = []
-for rr in r:
-    Hosts.append(json.dumps(rr, cls=AlchemyEncoder))
-print(json.dumps(Hosts))
+print(json.dumps(r, cls=AlchemyEncoder))
